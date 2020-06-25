72c438b14d4bf09a69af4a98ed8a51677b91e961
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2020-06-24 21:16:44 -0500

    Fixed issues with linting and added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This website is under construction, sorry! - <em>Nicholas Rose</em>
-        </p>
-      </header>
-    </div>
-  );
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    This website is under construction, sorry! -{' '}
+                    <em>Nicholas Rose</em>
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
