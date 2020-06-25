830284c94f54b6be6b6384d34fc29217fbc6a5a7
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2020-06-24 20:08:58 -0500

    Updates

---
+++ precache-manifest.bc83fab4de07e20c9ecfb20963c86348.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652c02936c8e22916892b4722e56ea67",
+    "url": "/rosa/index.html"
+  },
+  {
+    "revision": "70d225eb000f0d34deac",
+    "url": "/rosa/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "bc92c7a9c036019be5f5",
+    "url": "/rosa/static/js/2.54293d3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rosa/static/js/2.54293d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d225eb000f0d34deac",
+    "url": "/rosa/static/js/main.29cf9289.chunk.js"
+  },
+  {
+    "revision": "77914d7c63e86d12f8b0",
+    "url": "/rosa/static/js/runtime-main.ce7b63d9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/rosa/static/media/logo.ee7cd8ed.svg"
+  }
+]);
