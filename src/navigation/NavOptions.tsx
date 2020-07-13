a339066fef6511f3a2b673f78057c0d1c8f9b815
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2020-07-13 01:16:03 -0400

    Fix for 404 on github

--- src/navigation/NavOptions.tsx
+++ src/navigation/NavOptions.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import { Box, Button } from '@material-ui/core';
 
+function ignore(event: React.MouseEvent): void {
+    event.preventDefault();
+}
+
 function NavOptions(): JSX.Element {
     return (
         <Box alignItems="center">
-            <Button href="/home">Home</Button>
-            <Button href="/about">About Me (WIP)</Button>
-            <Button href="/resume">Resumé (WIP)</Button>
-            <Button href="/gallery">Gallery (WIP)</Button>
-            <Button href="/books">Books (WIP)</Button>
-            <Button href="/music">Music (WIP)</Button>
-            <Button href="/thoughts">Thoughts (WIP)</Button>
-            <Button href="/contact">Contact Me (WIP)</Button>
+            <Button href="/rosa/home" onClick={ignore}>
+                Home
+            </Button>
+            <Button href="/rosa/about" onClick={ignore}>
+                About Me (WIP)
+            </Button>
+            <Button href="/rosa/resume" onClick={ignore}>
+                Resumé (WIP)
+            </Button>
+            <Button href="/rosa/gallery" onClick={ignore}>
+                Gallery (WIP)
+            </Button>
+            <Button href="/rosa/books" onClick={ignore}>
+                Books (WIP)
+            </Button>
+            <Button href="/rosa/music" onClick={ignore}>
+                Music (WIP)
+            </Button>
+            <Button href="/rosa/thoughts" onClick={ignore}>
+                Thoughts (WIP)
+            </Button>
+            <Button href="/rosa/contact" onClick={ignore}>
+                Contact Me (WIP)
+            </Button>
         </Box>
     );
 }
