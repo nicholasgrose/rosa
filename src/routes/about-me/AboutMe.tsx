265f7fcdba467a4e461f14e7098fb6fd2e39b10c
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2020-08-15 00:04:12 -0400

    Began the aboutme

--- src/routes/about-me/AboutMe.tsx
+++ src/routes/about-me/AboutMe.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 
 function AboutMe(): JSX.Element {
-    return <Box>About Me</Box>;
+    return (
+        <Box>
+            <Typography variant="body1">
+                I suppose that one might say that my story begins in
+                Indianapolis, Indiana. That&apos;s where I was born after all. I
+                daresay that isn&apos;t where it started, though. It started
+                more fully in Fishers, what was, at the time, a smaller town to
+                the Northeast. It&apos;s now a city but that&apos;s besides the
+                point. There I was in elementary school until fourth grade. The
+                most memorable thing for me during this period is one particular
+                teach: Mr. Heiden. I apologize if you are reading this, Mr.
+                Heiden, and I misspelled your name. Nonetheless, he was a cool
+                guy. He travelled a lot and I credit him with first exposing me
+                to different cultures. He even introduced them to our classroom,
+                having us sit on the floor with lowered desks rather than
+                standard seating. It was a good time, and a fond memory of mine.
+            </Typography>
+        </Box>
+    );
 }
 
 export default AboutMe;
