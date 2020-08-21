d42de95712b00c05a795a504aa8c8974c326d35d
Arkinion <nicholasgr44@gmail.com>
2020-08-20 22:59:11 -0400

    Pretty animations!

--- src/routes/gallery/AlbumTitle.tsx
+++ src/routes/gallery/AlbumTitle.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTheme, Box } from '@material-ui/core';
+import { Box, Collapse } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 
 interface AlbumTitleProps {
@@ -8,25 +8,22 @@ interface AlbumTitleProps {
 
 function AlbumTitle(props: AlbumTitleProps): JSX.Element {
     const { hovering } = props;
-    const theme = useTheme();
-    const albumTitleTransition = {
-        height: hovering ? 'auto' : 0,
-        transition: theme.transitions.easing.easeInOut,
-    };
     const titleTypographyStyle = {
         fontWeight: 300,
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
     };
     return (
-        <Box style={albumTitleTransition}>
-            <Typography
-                align="center"
-                variant="h5"
-                component="h2"
-                style={titleTypographyStyle}
-            >
-                Portraits
-            </Typography>
+        <Box>
+            <Collapse in={hovering}>
+                <Typography
+                    align="center"
+                    variant="h5"
+                    component="h2"
+                    style={titleTypographyStyle}
+                >
+                    Portraits
+                </Typography>
+            </Collapse>
         </Box>
     );
 }
